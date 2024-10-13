4e1d4d025bea0287fec8510d82f2266d5add5300
Klinexsov <sekaalv@gmail.com>
2024-10-13 16:26:43 +0300

    app.py

--- app.py
+++ app.py
@@ -13,3 +13,9 @@ def about():
 # Удалите этот блок для запуска с gunicorn
 # if __name__ == '__main__':
 #     app.run(debug=True)
+
+@app.route('/')
+def home():
+    user_ip = request.remote_addr  # Получаем IP-адрес пользователя
+    print(f'User IP: {user_ip}')  # Печатаем IP-адрес в консоли
+    return render_template('index.html')
